3e4e4e751aa1036e7eed3f99df22c1f05a0978e1
Walmyr <walmyr@appear.in>
2017-04-12 19:05:42 +0200

    minor adjust on config file

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -24,7 +24,7 @@ module.exports.config = {
             "displaySpecDuration": true
         }));
     },
-    jasmineNodeOpts: {
+    "jasmineNodeOpts": {
         "defaultTimeoutInterval": 10000
     }
 };
