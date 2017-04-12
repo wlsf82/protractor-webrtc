e8e12a31143df7791b82ac681f47f7ab75f6fa6c
Walmyr <walmyr@appear.in>
2017-04-12 19:34:32 +0200

    break tests into two files and update config to look to all specs

--- test/moreClients.spec.js
+++ test/moreClients.spec.js
@@ -4,42 +4,11 @@ const EC = protractor.ExpectedConditions;
 const WebrtcSample = require("./webrtcSample.po");
 const DEFAULT_TIMEOUT = 5000;
 
-describe("WebRTC Sample", () => {
+describe("WebRTC Sample - more than one client", () => {
     const webrtcSample = new WebrtcSample();
 
     beforeEach(() => {
         browser.get("");
-    });
-
-    it("should show title", () => {
-        const title = "WebRTC Sample";
-
-        expect(browser.getTitle()).toEqual(title);
-        expect(webrtcSample.title.getText()).toEqual(title);
-    });
-
-    it("should show video element and buttons for 'snap', 'send' and 'send and snap'", () => {
-        expect(webrtcSample.videoCanvas.isDisplayed()).toBe(true);
-        expect(webrtcSample.snapButton.isDisplayed()).toBe(true);
-        expect(webrtcSample.sendButton.isDisplayed()).toBe(true);
-        expect(webrtcSample.snapAndSendButton.isDisplayed()).toBe(true);
-    });
-
-    it("should autoplay video be enabled", () => {
-        const isVideoAutoplayEnabled = browser.executeScript("const video = document.getElementById('camera'); return video.autoplay;");
-
-        expect(isVideoAutoplayEnabled).toBe(true);
-    });
-
-    it("should show header for incoming photos", () => {
-        expect(webrtcSample.incomingPhotosTitle.getText()).toEqual("Incoming photos");
-    });
-
-    it("should have the same room name on url and when returning it on console", () => {
-        const roomNameFromUrl = webrtcSample.getRoomNameFromUrl();
-        const roomNameFromConsole = browser.executeScript("return room;");
-
-        expect(roomNameFromUrl).toEqual(roomNameFromConsole);
     });
 
     it("should show incoming photo on browser 2 when browser 1 clicks 'snap and send' and they are in the same room", () => {
